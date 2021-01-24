1d4ef210b2b01951df3256e2ce02f57abb3dd6c8
ferror18 <ferror18@gmail.com>
2021-01-23 16:56:44 -0800

    Created router inside App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './views/Home';
+import { BrowserRouter, Route } from "react-router-dom";
+import { CssBaseline } from '@material-ui/core';
 
 function App() {
   return (
-    <Home/>
+    <>
+    <BrowserRouter>
+    <CssBaseline/>
+    <Route exact path='/' component={Home}/>
+    </BrowserRouter>
+    </>
   );
 }
 
 export default App;
+ 
